ea7a701b971d1743c9ea22afb5c5713cfd10ce65
Miach <37858521+MiachMih@users.noreply.github.com>
2022-11-04 21:25:22 -0400

    netlify changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  // other exports
+  target: "serverless", // add this line
+};
