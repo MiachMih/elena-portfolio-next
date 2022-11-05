4e35dbfec6b0d5672e60e9c05071ee68a4dbfbba
Miach <37858521+MiachMih@users.noreply.github.com>
2022-11-04 20:46:54 -0400

    Add title and description

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,28 @@
 import Link from "next/link";
 import styles from "./index.module.css";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      Choose your language
-      <Link className={styles.btn} href="/ru">
-        Русский
-      </Link>
-      <Link className={styles.btn} href="/en">
-        English
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Elena Coach</title>
+        <meta
+          name="description"
+          content="Providing quality coaching to help you choose your life"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={styles.container}>
+        Choose your language
+        <Link className={styles.btn} href="/ru">
+          Русский
+        </Link>
+        <Link className={styles.btn} href="/en">
+          English
+        </Link>
+      </div>
+    </>
   );
 }
