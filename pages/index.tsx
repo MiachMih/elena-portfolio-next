eaa0019d273b8c42c0f54aea56073f328e0f823b
Miach <37858521+MiachMih@users.noreply.github.com>
2022-11-04 20:12:17 -0400

    Add 404 page redirect and a landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
+import styles from "./index.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       Choose your language
-      <Link href="/ru">Русский</Link>
-      <Link href="/en">English</Link>
+      <Link className={styles.btn} href="/ru">
+        Русский
+      </Link>
+      <Link className={styles.btn} href="/en">
+        English
+      </Link>
     </div>
   );
 }
